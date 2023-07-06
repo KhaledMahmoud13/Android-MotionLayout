7b334e59f02f2fe6206b5589bc7b4fd9be166169
Khaled Mahmoud <khaled.ms.113@gmail.com>
2023-07-06 23:49:06 +0300

    commit

---
+++ app/src/main/java/com/khaled/motionlayout/Step7Activity.kt
@@ -0,0 +1,12 @@
+package com.khaled.motionlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity;
+
+class Step7Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step7)
+    }
+}
---
+++ app/src/main/java/com/khaled/motionlayout/Step8Activity.kt
@@ -0,0 +1,28 @@
+package com.khaled.motionlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.appbar.AppBarLayout
+
+class Step8Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step8)
+
+        coordinateMotion()
+    }
+
+    private fun coordinateMotion() {
+        val appBarLayout: AppBarLayout = findViewById(R.id.appbar_layout)
+        val motionLayout: MotionLayout = findViewById(R.id.motion_layout)
+
+        val listener = AppBarLayout.OnOffsetChangedListener { unused, verticalOffset ->
+            val seekPosition = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
+            motionLayout.progress = seekPosition
+        }
+
+        appBarLayout.addOnOffsetChangedListener(listener)
+    }
+}
